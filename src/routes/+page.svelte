75c7ebee38e85fce1c27b61ab8dd4cea8af85113
givensuman <givensuman@gmail.com>
2023-11-22 19:09:34 -0500

    Update svelte boilerplate

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	//@ts-ignore
+	import TiledCanvas from '../lib/TiledCanvas.js';
+
+	import { browser } from '$app/environment';
+
+	if (browser) {
+		let ctx = new TiledCanvas(document.getElementById('targetcanvas'));
+		ctx.context.normalMethod();
+		ctx.beginPath();
+		ctx.moveTo(5, 5);
+		ctx.lineTo(15, 15);
+		ctx.stroke();
+	}
+</script>
+
+<canvas id="whiteboard"></canvas>
